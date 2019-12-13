4210257dd24614a077d38ab0ae2dccdd086d50d1
Akash Singh <aakash.singh9246@yahoo.com>
2019-12-13 12:55:30 +0530

    adjancy final

--- DataStructureAndAlgorithm/Graph/adjacencyList.go
+++ DataStructureAndAlgorithm/Graph/adjacencyList.go
@@ -5,42 +5,49 @@ import (
 )
 
 type edge struct{
-	source 	int
-	dest	int
+	source 	byte
+	dest	byte
 	cost 	int
 	next	*edge
 }
 
 type edgeList struct{
-	head	map[byte]*edge
+	head	*edge
 }
 
 type graph struct{
 	count 		int
-	vertexList 	[]*edgeList
+	vertex   	[]byte
+	vertexList 	map[byte]*edgeList
 }
 
-func (g *graph)Init(cnt int){
+func (g *graph)Init(cnt int, vertex []byte){
 	g.count = cnt
-	g.vertexList = make([]*edgeList, cnt)
+	g.vertex = vertex
+	g.vertexList = make(map[byte]*edgeList, cnt)
 	for i:=0;i<cnt;i++{
-		g.vertexList[i]=new(edgeList)
-		g.vertexList[i].head=nil
+		g.vertexList[vertex[i]]=new(edgeList)
+		g.vertexList[vertex[i]].head=nil
 	}
 }
 
 func (g *graph)AddEdge(s,d byte, cost int){
-	edge := &edge{s,d,c,g.vertexList[s].head}
+	edge := &edge{s,d,cost,g.vertexList[s].head}
 	g.vertexList[s].head = edge
+}
+
+func (g *graph)AddBiDirEdge(s,d byte,cost int){
+	g.AddEdge(s,d,cost)
+	g.AddEdge(d,s,cost)
 }
 
 func (g *graph)Print(){
 	for i:=0;i<g.count;i++{
-		ad := g.vertexList[i].head
+		ad := g.vertexList[g.vertex[i]].head
 		if ad!=nil{
-			fmt.Print("vertex",i,"is connected to:")
+			fmt.Print("vertex ",string(g.vertex[i])," is connected to:")
 			for ad!=nil{
-				fmt.Print("[",ad.dest,ad.cost,"]")
+				fmt.Print("[",string(ad.dest)," ",ad.cost,"]")
 				ad=ad.next
 			}
 			fmt.Println()
@@ -50,6 +57,20 @@ func (g *graph)Print(){
 
 
 func main(){
-	
+	g:=&graph{}
+	ver := []byte{'A','B','C','D','E','F','G','H'}
+	g.Init(8,ver)
+	g.AddBiDirEdge('A','F',10)
+	g.AddBiDirEdge('A','C',5)
+	g.AddBiDirEdge('B','H',19)
+	g.AddBiDirEdge('B','F',9)
+	g.AddBiDirEdge('B','G',1)
+	g.AddBiDirEdge('C','B',6)
+	g.AddBiDirEdge('D','A',11)
+	g.AddBiDirEdge('D','G',3)
+	g.AddBiDirEdge('E','F',7)
+	g.AddBiDirEdge('E','H',8)
+	g.AddBiDirEdge('G','E',12)
+	g.AddBiDirEdge('H','C',15)
+	g.Print()
 }
-
