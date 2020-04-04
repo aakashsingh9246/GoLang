3e0d11cc02a23ed268b40552bcd762b2387c0877
Akash Singh <aakash.singh9246@yahoo.com>
2020-04-04 11:25:26 +0530

    introduced improved bubbleSorting

--- DataStructureAndAlgorithm/Sorting/BubbleSort.go
+++ DataStructureAndAlgorithm/Sorting/BubbleSort.go
@@ -27,3 +27,19 @@ func BubbleSort(data []int, comp more){
 		}
 	}
 }
+
+// improved bubble sort checks if swap is performed in current pass and decides list is sorted or not
+
+func BubbleSort(data []int, comp more){
+	size := len(data)
+	flag := true  // to check swap is performed 
+	for i:=0;i<size-1 && flag ;i++{
+		flag = false
+		for j:=0;j<size-i-1;j++{
+			if comp(data[j],data[j+1]){
+				data[j],data[j+1] = data[j+1],data[j]
+				flag = true
+			}
+		}
+	}
+}
